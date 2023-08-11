5c9c535fd087f2d6a56d058237cd1d9d677b0ed6
Bele208 <beelee208@gmail.com>
2023-08-11 01:16:46 -0300

    Primer commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import userRouter from './users.js';
+
+let router = express.Router();
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.use('/users', userRouter);
+export default router
