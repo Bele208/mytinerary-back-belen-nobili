24bf295514a6e0fd1f81fd450d1ca756716f3b8f
Belén Nobili <99286511+Bele208@users.noreply.github.com>
2023-08-23 02:10:04 -0300

    Carrusel (#4)
    * Varios
    
    * Ajustes varios y corrección de rutas

--- routes/cities.js
+++ routes/cities.js
@@ -4,6 +4,7 @@ import read from '../controllers/cities/read.js';
 import update from '../controllers/cities/update.js';
 import readOne from '../controllers/cities/readOne.js';
 import destroy from '../controllers/cities/destroy.js';
+import carrusel from '../controllers/cities/carrusel.js';
 
 
 let router = express.Router();
@@ -14,7 +15,8 @@ router.post('/', create)
 
 //READ
 router.get('/', read)
-router.get('/:user_id', readOne)
+router.get('/carrusel', carrusel)
+router.get('/:city_id', readOne)
 
 //UPDATE
 router.put('/:u_id', update)
