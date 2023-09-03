0e2399b3575e42f346dfa30b25ddc1d79a2c69f6
Bele208 <beelee208@gmail.com>
2023-09-03 14:32:22 -0300

    Modificaciones de itineraries

--- controllers/itineraries/read.js
+++ controllers/itineraries/read.js
@@ -1,20 +1,22 @@
 import Itinerary from "../../models/Itinerary.js";
 
-export default async(req,res, next) =>{
+export default async(req, res, next) =>{
     try {
-        let queries = {}
-        if (req.query.city_id){
+     let queries = {}
+         if (req.query.city_id){
             queries.city_id = req.query.city_id
-        }
-        let all = await Itinerary.find(queries, '-__v -createdAt -updatedAt')
+         }
+
+        let all = await Itinerary
+        .find(queries, '-__v -createdAt -updatedAt')
         .populate({
             path: "city_id",
             select: "city photo admin_id",
             populate:{
-                path : "admin_id",
-                select: "name"
+                 path : "admin_id",
+                select: "name photo"
             }
-        })
+         })
         return res.status(200).json({
             success: true,
             message: 'itineraries found',
